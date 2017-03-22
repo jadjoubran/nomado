9ae5756a5881c68ba97076765662e60e550d42ce
jadjoubran <joubran.jad@gmail.com>
2017-03-22 21:26:09 +0200

    online/offline

--- js/shell.js
+++ js/shell.js
@@ -1,4 +1,22 @@
 var splash = document.getElementById('splash');
-if (splash){
+if (splash) {
     splash.classList.add('hide');
+}
+
+window.addEventListener('online', function () {
+    console.log('online');
+    document.head.querySelector('meta[name=theme-color]').setAttribute('content', 'F44336')
+    document.body.classList.remove('offline');
+});
+
+window.addEventListener('offline', offlineMode);
+
+function offlineMode() {
+    console.log('offline');
+    document.head.querySelector('meta[name=theme-color]').setAttribute('content', '#353A49')
+    document.body.classList.add('offline');
+}
+
+if (!navigator.onLine) {
+    offlineMode();
 }
